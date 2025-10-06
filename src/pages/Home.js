35866c1848b568b4deb6859bf95d3d8f1a1df8a9
Sasindu Bandara <97359461+sasindub@users.noreply.github.com>
2025-10-06 23:43:43 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -739,7 +739,7 @@ const Home = () => {
             playsInline
             className="absolute inset-0 w-full h-full object-cover"
           >
-            <source src="/productionVid.mp4" type="video/mp4" />
+            <source src="https://www.youtube.com/watch?v=Z2MyXuHstIs" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           
