c0672d2052dea1538c855d48e65f0115ae87396d
Nimshi Silva <nimshisilva07@gmail.com>
2025-10-07 20:06:35 +0530

    change video link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -748,7 +748,7 @@ const Home = () => {
             playsInline
             className="absolute inset-0 w-full h-full object-cover"
           >
-            <source src="https://www.youtube.com/watch?v=Z2MyXuHstIs" type="video/mp4" />
+            <source src="/productionVid.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           
